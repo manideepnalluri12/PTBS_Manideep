e96cfaabee7d1595020a7bfb94264d62294c8287
manideepnalluri12 <mnallur2@asu.edu>
2022-10-17 23:55:09 -0700

    1st Commit

---
+++ src/OfferingList.java
@@ -0,0 +1,11 @@
+public class OfferingList {
+
+	private Trading trading;
+
+	private UserinfoItem userinfoItem;
+
+	private OfferingIterator offeringIterator;
+
+	private Offering[] offering;
+
+}
---
+++ src/classProductList.java
@@ -0,0 +1,13 @@
+public class classProductList {
+
+	private ReminderVistor reminderVistor;
+
+	private Product[] product;
+
+	private ProductIterator productIterator;
+
+	public void accept(NodeVisitor visitor) {
+
+	}
+
+}
